f9934b7ddcb10c408ffa188131cf934aa495c27b
dona126 <donamariamsunny@gmail.com>
2021-01-06 20:29:34 +0530

    third commit

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ const nav=[
 
 const booksRouter = require('./src/routes/bookRoutes')(nav);
 const authorsRouter = require('./src/routes/authorRoutes')(nav);
+const loginRouter = require('./src/routes/loginRoutes')(nav);
+const signupRouter = require('./src/routes/signupRoutes')(nav);
 
 //route handlers
 const app = express();
 app.use('/books', booksRouter);
 app.use('/authors', authorsRouter);
+app.use('/login', loginRouter);
+app.use('/signup',signupRouter);
 
 app.set('view engine', 'ejs'); 
 app.set('views', __dirname+'/src/views');
